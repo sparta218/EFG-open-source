0b02d015100d8e12249ce2d53f6cbb14123a90cc
ComLarsic <comlarsic@pop-os.localdomain>
2020-07-16 19:28:22 +0200

    Add RPG-PlayerConrtoller

---
+++ src/Player/Player.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+using EvilFarmingGame.Player;
+
+public class Player : KinematicBody2D
+{
+    protected PlayerController Controller;
+    
+    public override void _Ready()
+    {
+        Controller = new PlayerController(this);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Controller.InputMovement(delta);
+    }
+}
