71c3831f3733848eaeae7aa9d16edc68553a05e6
ComLarsic <comlarsic@pop-os.localdomain>
2020-07-22 10:36:05 +0200

    Added Day-Night Cycle, added another raycast to player that always ppoints up

--- src/Player/Player.cs
+++ src/Player/Player.cs
@@ -9,46 +9,52 @@ public class Player : KinematicBody2D
 {
 	private PlayerController Controller;
 	public Inventory Inventory;
-	
-	public RayCast2D RayCast;
-	public Node2D RayPivot;
-	
+
+	private Node2D RayPivot;
+
+	private RichTextLabel Clock;
+	public DayNight TimeNode;
+
 	private ResourcePreloader Loader = new ResourcePreloader();
+
 	public override void _Ready()
 	{
 		Controller = new PlayerController(this);
-		
-		RayPivot = GetNode<Node2D>("RayPivot");
-		RayCast = GetNode<RayCast2D>("RayPivot/RayCast2D");
-		
+
+		RayPivot = (Node2D) GetNode("RayPivot");
+		Clock = (RichTextLabel) GetNode("UI/ControlUI/Clock");
+
 		Inventory = new Inventory();
 		Inventory.Items.Add(Tools.BasicHoe);
+		
+		TimeNode = (DayNight) GetNode("DayNight");
 	}
 
 	public override void _PhysicsProcess(float delta)
 	{
-		Controller.InputMovement(delta,RayPivot);
-		InventoryHandling();
+		Controller.InputMovement(delta, RayPivot);
 		
-		RayCasting(RayCast);
+		InventoryHandling();
+		RayCasting();
+		TimeHandling();
 	}
 
 	public void InventoryHandling()
 	{
 		for (int i = 0; i < Inventory.Items.Count; i++)
 		{
-			Sprite Icon = (Sprite) GetNode($"UI/Inventory/Inventory Slot{i+1}/Item");
-			Sprite SelectBox = (Sprite) GetNode($"UI/Inventory/Inventory Slot{i+1}/Selection");
-			
-			if(Inventory[i] != null)
+			Sprite Icon = (Sprite) GetNode($"UI/Inventory/Inventory Slot{i + 1}/Item");
+			Sprite SelectBox = (Sprite) GetNode($"UI/Inventory/Inventory Slot{i + 1}/Selection");
+
+			if (Inventory[i] != null)
 				Icon.Texture = Inventory[i].Icon;
 		}
 
 		for (int i = 0; i < Inventory.Items.Capacity; i++)
-		{    
-			Sprite Icon = (Sprite) GetNode($"UI/Inventory/Inventory Slot{i+1}/Item");
-			Sprite SelectBox = (Sprite) GetNode($"UI/Inventory/Inventory Slot{i+1}/Selection");
-			
+		{
+			Sprite Icon = (Sprite) GetNode($"UI/Inventory/Inventory Slot{i + 1}/Item");
+			Sprite SelectBox = (Sprite) GetNode($"UI/Inventory/Inventory Slot{i + 1}/Selection");
+
 			if (i == Inventory.HeldSlot)
 				SelectBox.Show();
 			else
@@ -59,10 +65,10 @@ public class Player : KinematicBody2D
 				Icon.Texture = (Texture) GD.Load("res://src/NoTexture.png");
 			}
 		}
-		
+
 		if (Input.IsActionJustPressed("ui_right"))
 		{
-			if (Inventory.HeldSlot < Inventory.Items.Capacity -1)
+			if (Inventory.HeldSlot < Inventory.Items.Capacity - 1)
 				Inventory.HeldSlot++;
 		}
 		else if (Input.IsActionJustPressed("ui_left"))
@@ -70,34 +76,46 @@ public class Player : KinematicBody2D
 			if (Inventory.HeldSlot > 0)
 				Inventory.HeldSlot--;
 		}
-		
-		
+
+
 	}
 
-	public void RayCasting(RayCast2D RayCast)
+	public void RayCasting()
 	{
-		var collidedTile = RayCast.GetCollider();
-
-		switch (collidedTile)
+		foreach (RayCast2D RayCast in GetTree().GetNodesInGroup("PlayerRays"))
 		{
-			case FarmLand tile:
-				
-				var collidedFarmLand = (FarmLand)collidedTile;
-			
-				collidedFarmLand.OutLine.Visible = true;
-				collidedFarmLand.PlayerColliding = true;
-				collidedFarmLand.PlayerBody = this;
-				break;
-			
-			case SeedStorage tile:
-				
-				var collidedSeedStorage = (SeedStorage)collidedTile;
-			
-				collidedSeedStorage.OutLine.Visible = true;
-				collidedSeedStorage.PlayerColliding = true;
-				collidedSeedStorage.PlayerBody = this;
-				break;
+			var collidedTile = RayCast.GetCollider();
+
+			switch (collidedTile)
+			{
+				case FarmLand tile:
+
+					var collidedFarmLand = (FarmLand) collidedTile;
+
+					collidedFarmLand.OutLine.Visible = true;
+					collidedFarmLand.PlayerColliding = true;
+					collidedFarmLand.PlayerBody = this;
+					break;
+
+				case SeedStorage tile:
+
+					var collidedSeedStorage = (SeedStorage) collidedTile;
+
+					collidedSeedStorage.OutLine.Visible = true;
+					collidedSeedStorage.PlayerColliding = true;
+					collidedSeedStorage.PlayerBody = this;
+					break;
+			}
 		}
+
+	}
+
+	public void TimeHandling()
+	{
+		if(TimeNode.Afternoon)
+			Clock.BbcodeText = $"{TimeNode.TimeOfDay -12} PM";
+		else
+			Clock.BbcodeText = $"{TimeNode.TimeOfDay} AM";
 		
 	}
 }
