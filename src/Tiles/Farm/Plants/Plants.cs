56a2bd67c86ac4c739402f230b22c10eb37179ca
ComLarsic <ComLarsic-mail@protonmail.com>
2020-07-24 19:36:08 +0200

    Modified seed storage and added the ability to have multiple plants.

--- src/Tiles/Farm/Plants/Plants.cs
+++ src/Tiles/Farm/Plants/Plants.cs
@@ -4,6 +4,7 @@ namespace EvilFarmingGame.Objects.Farm.Plants
 {
     public struct Plants
     {
-        public static Plant TestPlant = new Plant("Test Plant", "A Simple seed used for testing and debugging purposes", 0, "res://src/Tiles/Farm/Plants/TestPlant/Texture1.png", "res://src/Tiles/Farm/Plants/TestPlant/Texture2.png", Crops.TestCrop);
+        public static Plant TestPlant = new Plant("Test Plant", "A Plant seed used for testing and debugging purposes", 0, "res://src/Tiles/Farm/Plants/TestPlant/Texture1.png", "res://src/Tiles/Farm/Plants/TestPlant/Texture2.png", Crops.TestCrop);
+        public static Plant TestPlant2 = new Plant("Test Plant2", "A Plant seed used for testing and debugging purposes", 1, "res://src/Tiles/Farm/Plants/TestPlant2/Texture1.png", "res://src/Tiles/Farm/Plants/TestPlant2/Texture2.png", Crops.TestCrop2);
     }
 }
